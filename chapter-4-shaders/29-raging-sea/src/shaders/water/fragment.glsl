f5154121c703cd19a2b2f240c95a7f07faa1dfb8
hhwrxhh <ivan.bohdan88y@gmail.com>
2024-01-31 16:56:42 +0200

    sea shader

---
+++ chapter-4-shaders/29-raging-sea/src/shaders/water/fragment.glsl
@@ -0,0 +1,17 @@
+
+uniform vec3 uDepthColor;
+uniform vec3 uSurfaceColor;
+uniform float uColorMultipier;
+uniform float uColorOffset;
+
+varying float vElevation;
+
+
+
+void main() {
+    #include <colorspace_fragment>
+
+    float mixStrength = (vElevation + uColorOffset) * uColorMultipier;
+    vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
+    gl_FragColor = vec4(color, 1.0 );
+}
